c1c9b91b349b1995c253ab1fcf4cefd92516e7df
Raikhuomon <rachaelaikhuomon@gmail.com>
2022-09-02 22:14:44 +0100

    Added a launch icon Added a new group activity

--- app/src/main/java/com/rachael/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/rachael/whatsappclone/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.rachael.whatsappclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_search -> {
+                Toast.makeText(this, "search click", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.new_group -> {
+                goToNewGroupActivity()
+                return true
+            }
+            R.id.new_broadcast -> {
+                goToNewGroupActivity2()
+                return true
+            }
+            R.id.Linked_devices -> {
+                Toast.makeText(this, "Linked Devices click", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.starred_messages -> {
+                Toast.makeText(this, "Starred Messages click", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.settings -> {
+            Toast.makeText(this, "Settings click", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+    private fun goToNewGroupActivity(){
+        val i = Intent(this, NewGroupActivity::class.java)
+        startActivity(i)
+    }
+    private fun goToNewGroupActivity2(){
+        val i = Intent(this, NewGroupActivity2::class.java)
+        startActivity(i)
     }
 }
