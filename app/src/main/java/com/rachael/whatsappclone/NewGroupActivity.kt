4c0d75bf0dbebf52f79db60403210d6156c11591
Raikhuomon <rachaelaikhuomon@gmail.com>
2022-09-04 21:05:28 +0100

    implementing list of friends

--- app/src/main/java/com/rachael/whatsappclone/NewGroupActivity.kt
+++ app/src/main/java/com/rachael/whatsappclone/NewGroupActivity.kt
@@ -2,6 +2,8 @@ package com.rachael.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class NewGroupActivity : AppCompatActivity() {
@@ -12,6 +14,11 @@ class NewGroupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_group)
 
         recyclerview = findViewById(R.id.listOfFriends)
+        recyclerview.adapter = listOfFriendsAdapter(this)
+        recyclerview.layoutManager = LinearLayoutManager(this)
 
+        //adding line decoration
+        val decor = DividerItemDecoration(this, RecyclerView.VERTICAL)
+        recyclerview.addItemDecoration(decor)
     }
 }
---
+++ app/src/main/java/com/rachael/whatsappclone/SampleData.kt
@@ -0,0 +1,11 @@
+package com.rachael.whatsappclone
+
+class SampleData {
+    val LIST_OF_FRIENDS: List<User> = listOf(
+        User("James Paul", "Hi I am alive"),
+        User("Ope Francis", "Hey I am using whatsapp"),
+        User("John isaac", "I am a pastor"),
+        User("Grace ike", "I believe in God"),
+        User("Talia ike", "The Lord is my light")
+    )
+}
